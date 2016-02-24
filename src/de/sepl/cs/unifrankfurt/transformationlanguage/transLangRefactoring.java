b04e3fc196cd1a3e2fad5773ce37e1d8c3fe6faa
Krishna <krishna.nm86@gmail.com>
2016-02-24 14:35:43 +0100

    All bits of search algo (except the crawling bit) implemented.

--- src/de/sepl/cs/unifrankfurt/transformationlanguage/transLangRefactoring.java
+++ src/de/sepl/cs/unifrankfurt/transformationlanguage/transLangRefactoring.java
@@ -5,12 +5,31 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.cdt.core.dom.ast.ASTNodeFactoryFactory;
 import org.eclipse.cdt.core.dom.ast.IASTName;
 import org.eclipse.cdt.core.dom.ast.IASTNode;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPNodeFactory;
+import org.eclipse.cdt.core.dom.parser.IScannerExtensionConfiguration;
+import org.eclipse.cdt.core.dom.parser.ISourceCodeParser;
+import org.eclipse.cdt.core.dom.parser.c.GCCScannerExtensionConfiguration;
+import org.eclipse.cdt.core.dom.parser.cpp.GPPScannerExtensionConfiguration;
 import org.eclipse.cdt.core.model.ICElement;
 import org.eclipse.cdt.core.model.ICProject;
+import org.eclipse.cdt.core.parser.FileContent;
+import org.eclipse.cdt.core.parser.IParserLogService;
+import org.eclipse.cdt.core.parser.IScanner;
+import org.eclipse.cdt.core.parser.IScannerInfo;
+import org.eclipse.cdt.core.parser.IncludeFileContentProvider;
+import org.eclipse.cdt.core.parser.NullLogService;
+import org.eclipse.cdt.core.parser.ParserLanguage;
+import org.eclipse.cdt.core.parser.ParserMode;
+import org.eclipse.cdt.core.parser.ScannerInfo;
 import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTBaseDeclSpecifier;
+import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTForStatement;
+import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTNamedTypeSpecifier;
+import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTSimpleDeclaration;
+import org.eclipse.cdt.internal.core.parser.scanner.CPreprocessor;
 import org.eclipse.cdt.internal.ui.refactoring.CRefactoring;
 import org.eclipse.cdt.internal.ui.refactoring.ModificationCollector;
 import org.eclipse.core.runtime.CoreException;
@@ -23,7 +42,9 @@ import org.eclipse.text.edits.TextEditGroup;
 
 @SuppressWarnings("restriction")
 public class transLangRefactoring extends CRefactoring {
-	// private List<Integer> forLoopLineNos = new ArrayList<Integer>();
+	private static List<Class> tempRules = new ArrayList<Class>();
+	private static ICPPNodeFactory nodeFactory = ASTNodeFactoryFactory.getDefaultCPPNodeFactory();
+	protected static final IParserLogService NULL_LOG = new NullLogService();
 	private IASTNode selectedNode = null;
 	private static Map<String, CPPASTBaseDeclSpecifier> typeDefinitionTransformations;
 	private static List<IASTNode> workQueue = new ArrayList<IASTNode>();
@@ -39,14 +60,69 @@ public class transLangRefactoring extends CRefactoring {
 		return null;
 	}
 
+	private void populateTempRules() {
+		tempRules.add(CPPASTForStatement.class);
+	}
+
+	private static List<IASTNode> createEnclosingNode(IASTNode node) {
+		List<IASTNode> returnNode = new ArrayList<IASTNode>();
+		IASTNode enclosingNode = node;
+		while (!(enclosingNode instanceof IASTTranslationUnit)) {
+			enclosingNode = enclosingNode.getParent();
+			if (ruleApplicable(enclosingNode)) {
+				returnNode.add(enclosingNode);
+				return returnNode;
+			}
+		}
+		enclosingNode = node;
+		if (enclosingNode instanceof CPPASTSimpleDeclaration
+				&& ((CPPASTSimpleDeclaration) enclosingNode).getDeclSpecifier() instanceof CPPASTNamedTypeSpecifier) {
+			IASTName name = ((CPPASTNamedTypeSpecifier) ((CPPASTSimpleDeclaration) enclosingNode).getDeclSpecifier())
+					.getName();
+			returnNode.add(TransformationUtils.getDefns(name));
+			returnNode.add(enclosingNode);
+		}
+		return returnNode;
+	}
+
+	private static boolean ruleApplicable(IASTNode enclosingNode) {
+		for (Class cls : tempRules) {
+			if (enclosingNode.getClass().equals(cls)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static IScanner getDummyScanner() {
+		FileContent codeReader = FileContent.create("<test-code>", "".toCharArray());
+		ScannerInfo scannerInfo = new ScannerInfo();
+		ISourceCodeParser parser2 = null;
+		IScanner scanner = createScanner(codeReader, ParserLanguage.CPP, ParserMode.COMPLETE_PARSE, scannerInfo);
+		return scanner;
+
+	}
+
+	public static IScanner createScanner(FileContent codeReader, ParserLanguage lang, ParserMode mode,
+			IScannerInfo scannerInfo) {
+		IScannerExtensionConfiguration configuration = null;
+		if (lang == ParserLanguage.C) {
+			configuration = GCCScannerExtensionConfiguration.getInstance(scannerInfo);
+		} else {
+			configuration = GPPScannerExtensionConfiguration.getInstance(scannerInfo);
+		}
+		IScanner scanner;
+		scanner = new CPreprocessor(codeReader, scannerInfo, lang, NULL_LOG, configuration,
+				IncludeFileContentProvider.getSavedFilesProvider());
+		return scanner;
+	}
+
 	@Override
 	public RefactoringStatus checkInitialConditions(IProgressMonitor pm)
 			throws CoreException, OperationCanceledException {
 		selectedNode = getAST(getTranslationUnit(), pm).getNodeSelector(null).findNode(selectedRegion.getOffset(),
 				selectedRegion.getLength());
-		// processSelectedNode(selectedNode, pm);
-		
-		selectedNode.accept(new NameVisitor());
+		/*selectedNode.accept(new NameVisitor());
 		System.out.println("Object Refs");
 		for (IASTName objectref : NameVisitor.getObjectrefs()) {
 			System.out.println(objectref.getRawSignature());
@@ -63,6 +139,13 @@ public class transLangRefactoring extends CRefactoring {
 			System.out.println("Defn");
 			System.out.println(TransformationUtils.getDefns(typeref).getRawSignature());
 
+		}*/
+		populateTempRules();
+		List<IASTNode> enclosingNodes =  createEnclosingNode(selectedNode);
+		for(IASTNode node : enclosingNodes)
+		{
+			System.out.println("------------------------");
+			System.out.println(node.getRawSignature());
 		}
 		return super.checkInitialConditions(pm);
 	}
@@ -70,24 +153,7 @@ public class transLangRefactoring extends CRefactoring {
 	@Override
 	protected void collectModifications(IProgressMonitor pm, ModificationCollector collector)
 			throws CoreException, OperationCanceledException {
-		final TextEditGroup editGroup = new TextEditGroup("Api Refactoring final modifications");
-		IASTTranslationUnit ast = getAST(tu, pm);
-		// ast.accept(new TTLVisitor(typeDefinitionTransformations,
-		// collector.rewriterForTranslationUnit(ast), editGroup));
-		/*
-		 * for (IASTPreprocessorStatement stat :
-		 * ast.getAllPreprocessorStatements()) { if (stat instanceof
-		 * IASTPreprocessorPragmaStatement) { String[] messages =
-		 * ((IASTPreprocessorPragmaStatement) stat).getRawSignature().split(" "
-		 * ); logger.info("Preprocessor message : " +
-		 * ((IASTPreprocessorPragmaStatement) stat).getRawSignature()); if
-		 * (messages[1].equalsIgnoreCase("vectorizeloop")) { try {
-		 * forLoopLineNos.add(stat.getFileLocation().getStartingLineNumber()); }
-		 * catch (Exception e) { logger.error(
-		 * "Pragma cannot be processed. Please supply number after #pragma vectorizeLoop"
-		 * ); } } ast.accept( new LoopVectorizeVisitor(forLoopLineNos,
-		 * collector.rewriterForTranslationUnit(ast), editGroup)); } }
-		 */
+
 	}
 
 }
